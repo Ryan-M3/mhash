b8a1fa24fa430250c9fbb8e8543707b335d94870
Ryan McNamara <gn341ram@gmail.com>
2018-11-24 17:51:55 -0800

    added moon logic

---
+++ src/MoonLogic.hs
@@ -0,0 +1,31 @@
+{-|
+Module     : MoonLogic
+Description: Implements "Dismal Arithmetic"
+License    : CC0
+Maintainer : Ryan McNamara <gn341ram@gmail.com>
+Portability: Linux
+
+For more information on dismal arithmetic, see:
+    https://www.numberphile.com/videos/lunar-arithmetic
+    https://arxiv.org/abs/1107.1130
+
+I'm  using  dismal  /  moon  arithmetic  because  a  single  hash
+algorithm of a  document is a dismal or moon  number, the minhash
+of two moon numbers is moon  addition and what we ultimately want
+to do is concat a document's moon numbers.
+|-}
+
+module MoonLogic where
+
+newtype MoonInt = MoonInt [Int]
+
+-- |Find the minimum of each component of an array of ints.
+minwise :: MoonInt -> MoonInt -> MoonInt
+minwise (MoonInt a) (MoonInt b) = MoonInt (zipWith min a b)
+
+instance Semigroup MoonInt where
+    a <> b = minwise a b
+
+instance Monoid MoonInt where
+    mempty  = MoonInt $ repeat maxBound
+    mappend = minwise
