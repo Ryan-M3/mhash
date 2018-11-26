80796c4f47602017380395c689f5801c079d6a28
Ryan McNamara <gn341ram@gmail.com>
2018-11-26 07:09:41 -0800

    Benchmarking became broken upon function renaming.

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,6 @@ import Criterion.Main
 
 main :: IO ()
 main = defaultMain benches
-    where benches = [ bgroup "parallel hash" [bench "10000" $ nfIO $ pHashDocs 8]
-                    , bgroup "serial hash"   [bench "10000" $ nfIO $ sHashDocs 8]
+    where benches = [ bgroup "parallel hash" [bench "10000" $ nfIO $ pHashDocs' 8]
+                    , bgroup "serial hash"   [bench "10000" $ nfIO $ sHashDocs  8]
                     ]
